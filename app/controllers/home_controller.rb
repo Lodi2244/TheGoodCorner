6c7605d645ea0dd89d5ba0ab9bf04848a836ad4c
Lodi2244 <elodieletellier1@gmail.com>
2019-05-21 18:33:01 +0200

    implement simlpe text search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
+
   def index
-    @offers = Offer.all
-    @offer_sample = @offers.sample
   end
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -5,4 +5,12 @@ class Offer < ApplicationRecord
   has_many :wishlists, through: :wished_offers
   has_many_attached :pictures
   accepts_nested_attributes_for :pictures_attachments, allow_destroy: true
+
+  def self.text_search(query)
+    if query.present?
+      where("title ilike :q or description ilike :q", q: "#{query}")
+    else
+      all
+    end
+  end
 end
