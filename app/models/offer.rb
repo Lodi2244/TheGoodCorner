5346bc86098184afec087f5361e59d499e8bfc5e
Lodi2244 <elodieletellier1@gmail.com>
2019-05-21 10:26:01 +0200

    configure aws bucket generate blob and attatchment migration

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,4 +3,6 @@ class Offer < ApplicationRecord
   belongs_to :owner, class_name: 'User', inverse_of: :offers
   has_many :wished_offers, dependent: :destroy
   has_many :wishlists, through: :wished_offers
+  has_many_attached :pictures
+  accepts_nested_attributes_for :pictures_attachments, allow_destroy: true
 end
