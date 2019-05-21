9a9ac349fcdea3cda3e75d6590f1d9f638af642d
Lodi2244 <elodieletellier1@gmail.com>
2019-05-21 15:48:28 +0200

    fix seed bug

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,6 +1,6 @@
 class Offer < ApplicationRecord
   belongs_to :category, optional: true
-  belongs_to :owner, class_name: 'User', foreign_key: :owner_id, optional: true
+  belongs_to :user, optional: true
   has_many :wished_offers, dependent: :destroy
   has_many :wishlists, through: :wished_offers
   has_many_attached :pictures
