77675d257e6d8c951b1cc560374075ae4e324b38
Lodi2244 <elodieletellier1@gmail.com>
2019-05-28 18:19:22 +0200

    [wip] navbar done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users
-  root 'offers#index'
+  devise_scope :user do
+  get 'users/show' => 'sessions#show'
+end
+  devise_for :users, controllers: {
+        sessions: 'users/sessions', registrations: 'users/registrations'
+      }
   resources :offers
   resources :advanced_searches
+  root 'offers#index'
 end
